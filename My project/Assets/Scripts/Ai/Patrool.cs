995f079d897ec7f5140ea6ca5afdd2fc787281c0
Cospero <117189288+Cospero@users.noreply.github.com>
2024-02-08 09:50:24 +0300

    AI fix
    FIx a same patrool loint bag

--- My project/Assets/Scripts/Ai/Patrool.cs
+++ My project/Assets/Scripts/Ai/Patrool.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,14 @@ public class Patrool : MonoBehaviour
     private List<PointOb> points = new List<PointOb>();
     private NavMeshAgent agent;
     private float timer=0;
+    private Transform _currentTarget;
+    private Transform _lastPatroolpoint;
 
     private void Start() 
     {
         agent=GetComponent<NavMeshAgent>();
         _pointsObject.GetComponentsInChildren<PointOb>(true,points);
+        _currentTarget=points[0].transform;
     }
 
     private void Update() 
@@ -24,17 +28,43 @@ public class Patrool : MonoBehaviour
         
         if(timer>2 & agent.remainingDistance<=agent.stoppingDistance) 
         {
-            StartPatrool();
-            timer=0;
+            Startpatrool();
+        }
+
+        if (_lastPatroolpoint==_currentTarget)
+        {
+            Startpatrool();
         }
         
         
     }
 
-    private void StartPatrool()
+    IEnumerator SetDestinationToPoint()
     {
-        agent.SetDestination(points[Random.Range(0,points.Count)].transform.position);
+        while(true)
+        {
 
+            yield return new WaitForSeconds(0.1f);
+            Debug.Log("Go to position");
+            agent.SetDestination(_currentTarget.position);
+        }
+    }
+
+    private void SetPatroolDestination()
+    {
+        _lastPatroolpoint=_currentTarget;
+        _currentTarget=points[UnityEngine.Random.Range(0,points.Count)].transform;
+        
+        StartCoroutine("SetDestinationToPoint");
+        //agent.SetDestination(points[Random.Range(0,points.Count)].transform.position);
+
+    }
+
+    private void Startpatrool()
+    {
+        StopCoroutine("SetDestinationToPoint");
+        SetPatroolDestination();
+        timer=0;
     }
 
 }
